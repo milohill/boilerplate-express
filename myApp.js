7d853f6595a67fed72540dd683db18a2e6360216
milohill <116620023+milohill@users.noreply.github.com>
2024-02-03 17:10:34 +0900

    finish the second prob

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
-console.log('Hello, world');
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
 
 module.exports = app;
