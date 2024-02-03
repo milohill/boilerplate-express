5aab67d0a25c11df32686458e83aab8068e6a623
milohill <116620023+milohill@users.noreply.github.com>
2024-02-03 18:33:47 +0900

    test 1

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/public/style.css`));
-
 app.get('/', (req, res) => {
   const path = `${__dirname}/views/index.html`;
   res.sendFile(path);
 });
+
+app.use(express.static(`${__dirname}/public/style.css`));
 
 module.exports = app;
