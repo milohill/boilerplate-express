74b22aecd9d4bdaed621fe1bf12052585b17c8c2
milohill <116620023+milohill@users.noreply.github.com>
2024-02-03 17:19:02 +0900

    fix the path

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const path = `${__dirname}views/index.html`;
+  const path = `${__dirname}/views/index.html`;
+  console.log(path);
   res.sendFile('/', path);
 });
 
